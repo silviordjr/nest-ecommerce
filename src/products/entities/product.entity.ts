36a0ee628a1dcfc5424605cfb1697002fb93bdcc
silviordjr <silviordjr@outlook.com>
2022-09-15 14:35:05 -0300

    criação das entidades e definição das tabelas

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'products' })
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: string;
+  @Column()
+  name: string;
+  @Column()
+  price: string;
+  @Column()
+  ownerId: string;
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+enum Role {
+  User = 'user',
+  Admin = 'admin',
+}
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: string;
+  @Column()
+  name: string;
+  @Column()
+  username: string;
+  @Column()
+  password: string;
+  @Column()
+  role: Role;
+}
